4fd2b77dbbcd058cefa16fd2041ba45d70cdc4ec
RuwanthiLakshika <daruwanthilakshika@gmail.com>
2024-02-13 15:11:09 +0530

    feat(Product): change2

--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -4,7 +4,23 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Product() {
   return (
-    <Card style={{ width: '25rem',border: 'none', borderBottom: '1px solid green',borderRadius: '0'  }}>
+    <>
+    <div className="row">
+    <Card className="container-fluid col-lg-5 " style={{border: 'none', borderBottom: '1px solid green',borderRadius: '0',marginleft:'15px',padding:'0'  }}>
+      <Card.Body>
+        <Card.Title>Cozy Cable Knit Sweater</Card.Title>
+        <Card.Text>
+        Code : F002{<br/>}Price : 1450.00 LKR
+        </Card.Text>
+        <Card.Text>
+        Keep warm and stylish with this cozy cable knit sweater, featuring a soft and comfortable material git 
+        perfect for chilly days.
+
+        </Card.Text>
+      </Card.Body>
+    </Card>
+    
+    <Card className="container-fluid col-lg-5" style={{border: 'none', borderBottom: '1px solid green',borderRadius: '0',padding:'0'   }}>
       <Card.Body>
         <Card.Title>Cozy Cable Knit Sweater</Card.Title>
         <Card.Text>
@@ -17,6 +33,11 @@ function Product() {
         </Card.Text>
       </Card.Body>
     </Card>
+    
+    </div>
+   
+    </>
+    
   );
 }
 
