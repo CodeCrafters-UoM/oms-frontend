d633a5eadf33735bd83032cd3eb46a9c07bc1a62
RuwanthiLakshika <daruwanthilakshika@gmail.com>
2024-02-14 17:51:46 +0530

    feat(Item-list): change1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes/routes";
+import ItemList from "./components/ItemList";
 
 function App() {
   return (
     <>
+      <ItemList/>
       <BrowserRouter>
         <AppRoutes />
       </BrowserRouter>
---
+++ src/components/ItemList.jsx
@@ -0,0 +1,122 @@
+import React from 'react';
+import Card from 'react-bootstrap/Card';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Product() {
+  return (
+    <>
+    <div className="row mt-5">
+        <div className="col-5">
+    <Card className="container-fluid col-lg-9 ms-15 p-0" style={{ border:'none', borderBottom: '1px solid green',borderRadius: '0' }}>
+      <Card.Body>
+        <Card.Title>Cozy Cable Knit Sweater</Card.Title>
+        <Card.Text>
+        Code : F002{<br/>}Price : 1450.00 LKR
+        </Card.Text>
+        <Card.Text>
+        Keep warm and stylish with this cozy cable knit sweater, featuring a soft and comfortable material git 
+        perfect for chilly days.
+
+        </Card.Text>
+      </Card.Body>
+    </Card>
+    </div>
+
+    <div className="col-2"></div>
+    
+    <div className="col">
+    <Card className="container-fluid col-lg-9 p-0 " style={{ border:'none',borderBottom: '1px solid green',borderRadius: '0'}}>
+      <Card.Body>
+        <Card.Title>Cozy Cable Knit Sweater</Card.Title>
+        <Card.Text>
+        Code : F002{<br/>}Price : 1450.00 LKR
+        </Card.Text>
+        <Card.Text>
+        Keep warm and stylish with this cozy cable knit sweater, featuring a soft and comfortable material 
+        perfect for chilly days.
+
+        </Card.Text>
+      </Card.Body>
+    </Card>
+    </div>
+    </div>
+   
+    <div className="row mt-5">
+        <div className="col-5">
+    <Card className="container-fluid col-lg-9 ms-15 p-0" style={{ border:'none',borderBottom: '1px solid green',borderRadius: '0' }}>
+      <Card.Body>
+        <Card.Title>Cozy Cable Knit Sweater</Card.Title>
+        <Card.Text>
+        Code : F002{<br/>}Price : 1450.00 LKR
+        </Card.Text>
+        <Card.Text>
+        Keep warm and stylish with this cozy cable knit sweater, featuring a soft and comfortable material git 
+        perfect for chilly days.
+
+        </Card.Text>
+      </Card.Body>
+    </Card>
+    </div>
+
+    <div className="col-2">
+        <button type="button" class="btn btn-success rounded-pill border btn-lg ps-5  pe-5 pt-1 pb-1">Add Item</button>
+    </div>
+
+    <div className="col-5">
+    <Card className="container-fluid col-lg-9 p-0" style={{ border:'none',borderBottom: '1px solid green',borderRadius: '0'}}>
+      <Card.Body>
+        <Card.Title>Cozy Cable Knit Sweater</Card.Title>
+        <Card.Text>
+        Code : F002{<br/>}Price : 1450.00 LKR
+        </Card.Text>
+        <Card.Text>
+        Keep warm and stylish with this cozy cable knit sweater, featuring a soft and comfortable material 
+        perfect for chilly days.
+
+        </Card.Text>
+      </Card.Body>
+    </Card>
+    </div>
+    </div>
+
+    <div className="row mt-5">
+        <div className="col-5">
+    <Card className="container-fluid col-lg-9 ms-15 p-0" style={{ border:'none',borderBottom: '1px solid green',borderRadius: '0' }}>
+      <Card.Body>
+        <Card.Title>Cozy Cable Knit Sweater</Card.Title>
+        <Card.Text>
+        Code : F002{<br/>}Price : 1450.00 LKR
+        </Card.Text>
+        <Card.Text>
+        Keep warm and stylish with this cozy cable knit sweater, featuring a soft and comfortable material git 
+        perfect for chilly days.
+
+        </Card.Text>
+      </Card.Body>
+    </Card>
+    </div>
+
+    <div className="col-2"></div>
+    
+    <div className="col-5">
+    <Card className="container-fluid col-lg-9 p-0" style={{ border:'none',borderBottom: '1px solid green',borderRadius: '0'}}>
+      <Card.Body>
+        <Card.Title>Cozy Cable Knit Sweater</Card.Title>
+        <Card.Text>
+        Code : F002{<br/>}Price : 1450.00 LKR
+        </Card.Text>
+        <Card.Text>
+        Keep warm and stylish with this cozy cable knit sweater, featuring a soft and comfortable material 
+        perfect for chilly days.
+
+        </Card.Text>
+      </Card.Body>
+    </Card>
+    </div>
+    </div>
+    </>
+    
+  );
+}
+
+export default Product
