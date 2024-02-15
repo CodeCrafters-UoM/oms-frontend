8a6fb1aabd7c852c927431beb73e69987d3452f6
RuwanthiLakshika <daruwanthilakshika@gmail.com>
2024-02-16 02:33:21 +0530

    feat(Item-Details): change1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes/routes";
+import ItemDetails from "./components/ItemDetails";
 
 function App() {
   return (
     <>
+      <ItemDetails />
       <BrowserRouter>
         <AppRoutes />
       </BrowserRouter>
