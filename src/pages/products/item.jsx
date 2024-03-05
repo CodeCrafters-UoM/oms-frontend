f483c692b96b3e4612e697d9e0ad3a3d44ac5546
RuwanthiLakshika <daruwanthilakshika@gmail.com>
2024-03-06 04:55:14 +0530

    feat(Item-Component): change2

--- src/pages/products/item.jsx
+++ src/pages/products/item.jsx
@@ -2,23 +2,21 @@ import React from 'react';
 import Card from 'react-bootstrap/Card';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function Product() {
+function Item({ item }) { // Receive item details as props
   return (
     <>
-      <Card key={item.id} className="container-fluid col-lg-9 ms-15 p-0" style={{ border:'none',borderBottom: '1px solid green',borderRadius: '0' }}>
-          <Card.Body>
-            <Card.Title>{item.name}</Card.Title>
-            <Card.Text>
-              Code : {item.code} <br />
-              Price : {item.price} LKR
-            </Card.Text>
-            <Card.Text>{item.description}</Card.Text>
-          </Card.Body>
-        </Card>
-   
+      <Card className="container-fluid col-lg-9 ms-15 p-0" style={{ border:'none',borderBottom: '1px solid green',borderRadius: '0' }}>
+        <Card.Body>
+          <Card.Title>{item.name}</Card.Title>
+          <Card.Text>
+            Code : {item.code} <br />
+            Price : {item.price} LKR
+          </Card.Text>
+          <Card.Text>{item.description}</Card.Text>
+        </Card.Body>
+      </Card>
     </>
-    
   );
 }
 
-export default Product
+export default Item;
