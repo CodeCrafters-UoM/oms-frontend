b8cdb178f085bfeb0bb0f8b6779412e3e51af588
Pramoth Dilshan <pramothdilshan2001@gmail.com>
2024-03-15 10:04:11 +0530

    feat: install bash

--- src/pages/products/Item.jsx
+++ src/pages/products/Item.jsx
@@ -1,21 +1,27 @@
-import React from 'react';
-import Card from 'react-bootstrap/Card';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import Card from "react-bootstrap/Card";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function Item({ item }) { // Receive item details as props
+function Item({ item }) {
+  // Receive item details as props
   return (
-    <>
-      <Card className="container-fluid col-lg-9 ms-15 p-0" style={{ border:'none',borderBottom: '1px solid green',borderRadius: '0' }}>
-        <Card.Body>
-          <Card.Title>{item.name}</Card.Title>
-          <Card.Text>
-            Code : {item.productCode} <br />
-            Price : {item.price} LKR
-          </Card.Text>
-          <Card.Text>{item.description}</Card.Text>
-        </Card.Body>
-      </Card>
-    </>
+    <Card
+      className="container-fluid col-lg-12 ms-15 p-0"
+      style={{
+        border: "none",
+        borderBottom: "1px solid green",
+        borderRadius: "0",
+      }}
+    >
+      <Card.Body className="ps-0">
+        <Card.Title>{item.name}</Card.Title>
+        <Card.Text>
+          Code : {item.productCode} <br />
+          Price : {item.price} LKR
+        </Card.Text>
+        <Card.Text>{item.description}</Card.Text>
+      </Card.Body>
+    </Card>
   );
 }
 
